ad3881d0bc84da1f02243838eb148be03df83670
Olle Wreede <olle@wreede.se>
2023-03-03 15:08:42 +0100

    Add vercel installation script

---
+++ bin/vercel/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+PATH=$PATH:/vercel/.cargo/bin
+
+rustup target add wasm32-unknown-unknown
+cargo install wasm-bindgen-cli
+cargo install --locked trunk
